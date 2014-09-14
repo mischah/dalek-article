cdf8780c1b0dcd097ee3b0f2998c6b8e38edde3c
Michael Kühnel <m.kuehnel@micromata.de>
2014-09-14 10:53:12 +0200

    Add test making screenshots

--- tests/firstTests.js
+++ tests/firstTests.js
@@ -7,6 +7,10 @@ var homepage = 'https://github.com/',
 	search = {
 		searchTerm: 'Micromata',
 		companyName: 'Micromata GmbH'
+	},
+	breakpoints = {
+		xLarge: 1280,
+		large: 1024
 	};
 
 module.exports = {
@@ -69,5 +73,18 @@ module.exports = {
 			.assert.text('h1.org-name span', search.companyName, 'Firmenname ist korrekt')
 
 			.done();
-	}
+	},
+	'Visually check breakpoints': function (test) {
+		test
+			.open(homepage)
+			.waitForElement('body')
+			.resize({width: breakpoints.large, height: 1024})
+			.screenshot('tests/screenshots/breakpoint-' + breakpoints.large + '-' + new Date().getTime() + '.png')
+			.assert.width('body', breakpoints.large, 'Fensterbreite auf ' + breakpoints.large + 'px geändert')
+			.resize({width: breakpoints.xLarge, height: 1024})
+			.screenshot('tests/screenshots/breakpoint-' + breakpoints.xLarge + '-' + new Date().getTime() + '.png')
+			.assert.width('body', breakpoints.xLarge, 'Fensterbreite auf ' + breakpoints.xLarge + 'px geändert')
+
+			.done();
+	},
 };
